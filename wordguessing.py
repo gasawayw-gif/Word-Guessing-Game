88f55c1cae9b3e9fbb83837b7adc4dff5ec8d781
Will Gasaway <gasawayw@uni.edu>
2026-05-12 17:15:09 -0500

    Fixed errors found by linter

--- wordguessing.py
+++ wordguessing.py
@@ -1,6 +1,13 @@
 import random
-word_bank = ['Strawberry', 'Eclipse', 'Chandelier', 'Ketchup', 'Toothpaste', 'Rainbow', 'Boardgame', 'Beehive', 'Lemon', 'Wreath', 'Waffles', 'Bubble', 'Whistle', 'Snowball', 'Bouquet', 'Headphones', 'Fireworks', 'Igloo', 'Ferris wheel', 'Banana peel', 'Lawnmower', 'Summer', 'Whisky', 'Cupcake', 'Sleeping bag', 'Bruise', 'Fog', 'Crust', 'Battery', 'Paris', 'Beach', 'Mountains', 'Hawaii', 'Mount Rushmore', 'USA', 'Hospital', 'Attic', 'Japan', 'Library', 'Desert', 'Mars', 'Washington DC', 'Las Vegas', 'Train station', 'North Pole', 'Farm', 'Disney World', 'Mexico', 'Giraffe', 'Koala', 'Wasp', 'Scorpion', 'Lion', 'Salamander', 'Dolphin', 'Frog', 'Panda', 'Platypus', 'T rex', 'Meerkat', 'Eagle', 'Mailman', 'Superman', 'Justin Beiber', 'Cowboy', 'Alexander Hamilton', 'Robin Hood', 'Vampire', 'Pirate', 'Girl Scout', 'Pikachu', 'Spongebob', 'Baby Yoda', 'Pilgrim', 'Cinderella', 'Baker', 'Lincoln', 'Thief', 'Leprechaun', 'Harry Potter', 'Shrek', 'Yoshi', 'Queen Elizabeth', 'Skip', 'Burp', 'Cook', 'Scratch', 'Sleep', 'Plant', 'Purchase', 'Text', 'Tie', 'Snore', 'Catch', 'Study', 'Olympics', 'Sandcastle', 'Recycle', 'Black hole', 'Applause', 'Blizzard', 'Sunburn', 'Time machine', 'Lace', 'Monday', 'Atlantis', 'Swamp', 'Panama', 'Canal', 'Sunscreen', 'Dictionary', 'Vanilla', 'Century']
-#this are all the words that can be chosen
+word_bank = ['Strawberry', 'Eclipse', 'Chandelier', 'Ketchup', 'Toothpaste', 'Rainbow', 'Boardgame', 'Beehive', 'Lemon', 'Wreath', 'Waffles', 'Bubble', 'Whistle', 'Snowball', 'Bouquet',
+             'Headphones', 'Fireworks', 'Igloo', 'Ferris wheel', 'Banana peel', 'Lawnmower', 'Summer', 'Whisky', 'Cupcake', 'Sleeping bag', 'Bruise', 'Fog', 'Crust', 'Battery', 'Paris', 
+             'Beach', 'Mountains', 'Hawaii', 'Mount Rushmore', 'USA', 'Hospital', 'Attic', 'Japan', 'Library', 'Desert', 'Mars', 'Washington DC', 'Las Vegas', 'Train station', 'North Pole', 
+             'Farm', 'Disney World', 'Mexico', 'Giraffe', 'Koala', 'Wasp', 'Scorpion', 'Lion', 'Salamander', 'Dolphin', 'Frog', 'Panda', 'Platypus', 'T rex', 'Meerkat', 'Eagle', 'Mailman',
+             'Superman', 'Justin Beiber', 'Cowboy', 'Alexander Hamilton', 'Robin Hood', 'Vampire', 'Pirate', 'Girl Scout', 'Pikachu', 'Spongebob', 'Baby Yoda', 'Pilgrim', 'Cinderella', 'Baker',
+             'Lincoln', 'Thief', 'Leprechaun', 'Harry Potter', 'Shrek', 'Yoshi', 'Queen Elizabeth', 'Skip', 'Burp', 'Cook', 'Scratch', 'Sleep', 'Plant', 'Purchase', 'Text', 'Tie', 'Snore', 'Catch',
+             'Study', 'Olympics', 'Sandcastle', 'Recycle', 'Black hole', 'Applause', 'Blizzard', 'Sunburn', 'Time machine', 'Lace', 'Monday', 'Atlantis', 'Swamp', 'Panama', 'Canal', 'Sunscreen',
+             'Dictionary', 'Vanilla', 'Century']
+# this are all the words that can be chosen
 
 play_again = "yes"
 
@@ -8,13 +15,13 @@ while play_again == "yes":
 
     word = random.choice(word_bank)
     word_lower = word.lower()
-    #this makes the word chosen completely random
+    # this makes the word chosen completely random
 
     guessedWord = [' ' if char == ' ' else '_' for char in word]
-    #makes the word "invisible" (_)
+    # makes the word "invisible" (_)
 
     attempts = 10
-    #attempts to guess letters of the word before failure
+    # attempts to guess letters of the word before failure
 
     while attempts > 0:
         print('\nCurrent word: ' + ''.join(guessedWord))
